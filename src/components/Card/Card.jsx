313c99549dd824819ac3ca965babccb3bc47ee89
MarynaPyvovar <marina18renkas@gmail.com>
2023-03-03 14:13:12 +0200

    add number format

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,14 @@ import Button from 'components/Button/Button';
 
 function Card({ data }) {
     const { id, user, isFollowed, tweets, followers, avatar } = data;
+    
+    const formatedFollowers = followers.toLocaleString('en-US');
+
     return (
         <li>
             <img src={avatar} alt={user} />
             <p>{tweets} tweets</p>
-            <p>{followers} {followers === 1 ? "follower" : "followers"}</p>
+            <p>{formatedFollowers} {followers === 1 ? "follower" : "followers"}</p>
             <Button isFollowed={isFollowed} id={id} />
         </li>
     )
